dff1100cc231ac6c359851347719de1c85f4a99e
Messai-Yosra <126110467+Messai-Yosra@users.noreply.github.com>
2023-08-27 13:11:54 +0100

    boucles

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ var myname ="Test"
 console.log(myname);
 myname = 404;
 console.log(myname);
+
+
+// ========== conditions =========
+ 
+var score = 80 
+if (score >= 70 ){
+    console.log ("exam taada "  )
+}
+
+// ======= for ======
+for ( var i =0; i <10; i++)
+{
+console.log(i);
+
+}
+
+
+// =========== array ======
+var names =["ahmed",2,"ali","sarra"];
+console.log(names[1] );
+
+
+// ========= object======
+var car = {
+    type :"AUDI",
+    color :"BLACK",
+    speed : "1245"
+
+};
+console.log(car.type );
+
+
+
